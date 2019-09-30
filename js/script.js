bc71b859577f3bff3a58136517d8ada62668da97
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-09-30 16:09:34 -0300

    cria lógica de validação de campos vazios

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 const formulario = document.getElementById("formulario");
 const inputTarefa = document.getElementById("inputTarefa");
 const tarefasAdicionadas = document.getElementById("tarefasAdicionadas");
+const erro = document.getElementById("erro")
 
 formulario.addEventListener("submit", function(evento){
     evento.preventDefault();
     const valorInput = inputTarefa.value;
-    const novaTarefa = document.createElement("p");
-    tarefasAdicionadas.appendChild(novaTarefa);
-    novaTarefa.textContent = valorInput;
+    if (valorInput.trim() === ""){
+        erro.textContent = "Preencha o campo!";
+    } else {
+        erro.textContent = "";
+        const novaTarefa = document.createElement("p");
+        tarefasAdicionadas.appendChild(novaTarefa);
+        novaTarefa.setAttribute("id", "realizada");
+        novaTarefa.textContent = valorInput;
+    }
     inputTarefa.value = "";
 });
+
